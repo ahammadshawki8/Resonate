009164dfb7ca5cdfb5af5cfedd8cb044bbdfc581
Ahammad Shawki <ahammadshawki8@gmail.com>
2026-01-30 10:49:30 +0600

    pass open 17

--- resonate_server/resonate_server_server/bin/main.dart
+++ resonate_server/resonate_server_server/bin/main.dart
@@ -2,6 +2,10 @@ import 'package:resonate_server_server/server.dart';
 
 /// This is the starting point for your Serverpod server. Typically, there is
 /// no need to modify this file.
+import 'dart:io';
+
 void main(List<String> args) {
+  print('SERVERPOD_CONFIG env: [32m[1m[4m[7m' + (Platform.environment['SERVERPOD_CONFIG'] ?? 'NOT SET') + '\u001b[0m');
+  print('Args: $args');
   run(args);
 }
